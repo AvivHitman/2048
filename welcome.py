79cd34935e595e8c5e8d3a52445d2baa9049f1d6
lirshindalman <lir@texel.live>
2021-01-19 13:17:39 +0200

    fix

--- welcome.py
+++ welcome.py
@@ -1,13 +1,15 @@
 import tkinter as tk
+from tkinter import *
+
 import colors as c
 from game import Game
 
 
 class Welcome(tk.Frame):
     flag = 0
-    newWindow = None
+    new_window = None
+    
     def __init__(self, master):
-
         self.master = master
         self.frame = tk.Frame(self.master)
         self.master.title('2048')
@@ -16,26 +18,26 @@ class Welcome(tk.Frame):
             self, bg=c.GRID_COLOR, bd=5, width=400, height=400)
         self.main_grid.grid(pady=(0, 0))
         self.grid()
-        button1 = tk.Button(self, text='New Game', command=lambda: self.new_window())
+        button1 = tk.Button(self, text='New Game', command=lambda: self.create_new_window())
         button1.grid(row=0, column=0, padx=10, pady=10)
         button1.config(height=2, width=15)
 
-
     def on_close(self):
-        self.destroy()
+        self.master.destroy()
 
-    def new_window(self):
+    def create_new_window(self):
         try:
-
             if (self.flag  == 0):
-                self.newWindow = tk.Toplevel(self.master)
-            frame = Game(self.newWindow, self)
+                self.new_window = tk.Toplevel(self.master)
+
+            frame = Game(self.new_window, self)
             frame.tkraise()
             self.flag = 1
+
         except:
-            print("You cannot create another Singleton class")
+            print("You cannot create another Game window")
             text = tk.Label(self.master, bg=c.WARNING, text="You cannot open more then one game!")
             text.grid(row=1, column=0, padx=0, pady=10)
             text.config(height=2, width=40)
 
-        self.master.protocol("WM_DELETE_WINDOW", self.on_close())
+        self.new_window.protocol("WM_DELETE_WINDOW", self.on_close)
