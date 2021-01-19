d9b85390d4f2f17542bf82a0d95c58a66ec76f4c
aviv tfilin <avivt193@gmail.com>
2021-01-19 13:04:28 +0200

    close all windows

--- welcome.py
+++ welcome.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import *
+
 import colors as c
 from game import Game
 
@@ -6,8 +8,8 @@ from game import Game
 class Welcome(tk.Frame):
     flag = 0
     newWindow = None
-    def __init__(self, master):
 
+    def __init__(self, master):
         self.master = master
         self.frame = tk.Frame(self.master)
         self.master.title('2048')
@@ -21,21 +23,24 @@ class Welcome(tk.Frame):
         button1.config(height=2, width=15)
 
 
-    def on_close(self):
-        self.destroy()
 
     def new_window(self):
         try:
 
             if (self.flag  == 0):
                 self.newWindow = tk.Toplevel(self.master)
+
             frame = Game(self.newWindow, self)
             frame.tkraise()
             self.flag = 1
+
         except:
-            print("You cannot create another Singleton class")
+            print("You cannot create another Game window")
             text = tk.Label(self.master, bg=c.WARNING, text="You cannot open more then one game!")
             text.grid(row=1, column=0, padx=0, pady=10)
             text.config(height=2, width=40)
 
-        self.master.protocol("WM_DELETE_WINDOW", self.on_close())
+        def on_close():
+            self.master.destroy()
+
+        self.newWindow.protocol("WM_DELETE_WINDOW", on_close)
